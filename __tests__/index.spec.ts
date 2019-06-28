7b07b4026a90960c18df6b72e9244e2e514c7e83
Øyvind Hermansen <oyvindhermansen@yvinds-MacBook-Pro.local>
2019-06-28 13:26:43 +0200

    init

---
+++ __tests__/index.spec.ts
@@ -0,0 +1,43 @@
+import Vasadu from '../lib/index';
+
+const tree = {
+  buttonText: {
+    publish: {
+      no: 'Publiser',
+      en: 'Publish'
+    }
+  },
+  branding: {
+    no: 'Mitt varemerke',
+    en: 'My brand!'
+  }
+};
+
+describe('vasadu', () => {
+  it('should translate to norwegian with no locale', () => {
+    const localizer = new Vasadu(tree, 'no');
+    expect(localizer.si('buttonText.publish')).toEqual('Publiser');
+  });
+
+  it('should translate to english with en locale', () => {
+    const localizer = new Vasadu(tree, 'en');
+    expect(localizer.si('buttonText.publish')).toEqual('Publish');
+  });
+
+  it('should return a message that indicates empty locale when locale is not defined', () => {
+    const localizer = new Vasadu(tree, '');
+    expect(localizer.si('buttonText.publish')).toEqual('Missing translation');
+  });
+
+  it('should throw if localization tree is not a plain object', () => {
+    expect(() => {
+      // @ts-ignore
+      new Vasadu('hey', '');
+    }).toThrow();
+
+    expect(() => {
+      // @ts-ignore
+      new Vasadu([{ no: 'hey', en: 'test' }], '');
+    }).toThrow();
+  });
+});
---
+++ lib/index.ts
@@ -0,0 +1,37 @@
+import * as objectPath from 'object-path';
+
+interface Localize {
+  [key: string]: string | Localize;
+}
+
+export default class Vasadu {
+  private tree: Localize;
+  private locale: string;
+
+  constructor(tree: Localize, locale: string) {
+    this.tree = tree;
+    this.locale = locale;
+
+    if (typeof tree !== 'object' || Array.isArray(this.tree)) {
+      throw new Error(`The localization tree must be a plain object.`);
+    }
+  }
+
+  /**
+   * Si - the method you use when wanting to access a
+   * translated value inside a localization tree.
+   * @param path String
+   * @returns String
+   */
+  si(path: string): string {
+    const values = objectPath.get(this.tree, path);
+    const emptyTranslation = 'Missing translation';
+
+    if (!(this.locale in values)) {
+      console.warn(`Missing translation for path: '${path}'`);
+      return emptyTranslation;
+    }
+
+    return values[this.locale];
+  }
+}
